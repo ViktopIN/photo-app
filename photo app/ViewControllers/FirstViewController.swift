2f03a98377efea8a401c9905a23f108b14fd90c9
Viktor Pinyagin <viktopiniaginya@gmail.com>
2022-06-29 18:23:14 +0500

    Create ViewControllers
    Create ViewControllers  for tabbar items, and set tabbar background

---
+++ photo app/ViewControllers/FirstViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.swift
+//  photo app
+//
+//  Created by t h a . m a m e rozz on 29.06.22.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
